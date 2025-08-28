0281412dd521168a46f65f9b2c5423fb00342b36
Rafael Magalhães <rafaelgabriel20@hotmail.com>
2025-08-28 19:40:59 -0300

    Estrutura for

---
+++ Aula 14/Repetições.js
@@ -0,0 +1,17 @@
+// usando while para contar 1 a 10
+/*var c = 1
+while (c <= 10){
+    c++
+}*/
+
+
+// usando o for para contar de 1 a 10
+//sendo for minha estrutura
+// meu inicio sendo a variavel c que recebe 1
+// teste logico sendo c menor ou igual a 10
+// meu incremento sendo o valor de c + 1 somando até chegar a 10
+console.log('Vai comerçar .....')
+for (var c = 1; c<=10;c++){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
